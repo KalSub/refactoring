f5710183613f00f06b6d60d3b3a26fb7baaaa9f2
KalSub <kalan.subieta@mail.com>
2025-11-19 20:00:09 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,28 @@
 package theater;
 
+/**
+ * Play class representing a play.
+ */
 public class Play {
+    private final String name;
+    private final String type;
 
-    public String name;
-    public String type;
-
+    /**
+     * Constructs a Play with the given name and type.
+     *
+     * @param name the name of the play
+     * @param type the type of the play (e.g., comedy, tragedy, etc.)
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
